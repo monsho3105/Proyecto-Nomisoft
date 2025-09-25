7ede9ebbf455969e766ee9a17ad5be1e83c91a0b
monsho3105 <yeisonhernandez9a@gmail.com>
2025-09-25 08:03:14 -0500

    Add project files.

---
+++ Proyecto Nomisoft/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Proyecto_Nomisoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnMinimzar = new System.Windows.Forms.PictureBox();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimzar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUser.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.txtUser.Location = new System.Drawing.Point(469, 124);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(427, 50);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "USUARIO";
+            this.txtUser.Enter += new System.EventHandler(this.txtUser_Enter);
+            this.txtUser.Leave += new System.EventHandler(this.txtUser_Leave);
+            this.txtUser.MouseEnter += new System.EventHandler(this.txtUser_MouseEnter);
+            this.txtUser.MouseLeave += new System.EventHandler(this.txtUser_MouseLeave);
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPass.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.ForeColor = System.Drawing.Color.LightGray;
+            this.txtPass.Location = new System.Drawing.Point(469, 267);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(427, 50);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Text = "CONTRASEÑA";
+            this.txtPass.Enter += new System.EventHandler(this.txtPass_Enter);
+            this.txtPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPass_KeyPress);
+            this.txtPass.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtPass_KeyUp);
+            this.txtPass.Leave += new System.EventHandler(this.txtUser_Leave);
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnAcceder.FlatAppearance.BorderSize = 0;
+            this.btnAcceder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnAcceder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcceder.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.btnAcceder.Location = new System.Drawing.Point(469, 427);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(427, 50);
+            this.btnAcceder.TabIndex = 3;
+            this.btnAcceder.Text = "ACCEDER";
+            this.btnAcceder.UseVisualStyleBackColor = false;
+            this.btnAcceder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.Location = new System.Drawing.Point(597, 498);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(168, 20);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Recuperar contraseña";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnMinimzar
+            // 
+            this.btnMinimzar.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimzar.BackgroundImage = global::Proyecto_Nomisoft.Properties.Resources.icono_minimizar;
+            this.btnMinimzar.Image = global::Proyecto_Nomisoft.Properties.Resources.icono_minimizar;
+            this.btnMinimzar.Location = new System.Drawing.Point(914, 12);
+            this.btnMinimzar.Name = "btnMinimzar";
+            this.btnMinimzar.Size = new System.Drawing.Size(34, 34);
+            this.btnMinimzar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimzar.TabIndex = 6;
+            this.btnMinimzar.TabStop = false;
+            this.btnMinimzar.Click += new System.EventHandler(this.btnMinimzar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCerrar.BackgroundImage = global::Proyecto_Nomisoft.Properties.Resources.boton_cerrar;
+            this.btnCerrar.Image = global::Proyecto_Nomisoft.Properties.Resources.boton_cerrar;
+            this.btnCerrar.Location = new System.Drawing.Point(954, 12);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(34, 34);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.btnMinimzar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimzar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.PictureBox btnMinimzar;
+    }
+}
+
---
+++ Proyecto Nomisoft/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Nomisoft
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUser_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtUser_Leave(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void txtUser_MouseEnter(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "USUARIO")
+            {
+                txtUser.Text = "";
+                txtUser.ForeColor = Color.WhiteSmoke;
+
+            }
+        }
+
+        private void txtUser_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "")
+            {
+                txtUser.Text = "USUARIO";
+                txtUser.ForeColor = Color.WhiteSmoke;
+
+            }
+        }
+
+        private void txtPass_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtPass_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "CONTRASEÑA")
+            {
+                txtPass.Text = "";
+                txtPass.ForeColor = Color.WhiteSmoke;
+                txtPass.UseSystemPasswordChar = true;  
+            }
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPass.Text != "CONTRASEÑA" && txtPass.Text.Length > 0)
+            {
+                txtPass.UseSystemPasswordChar = true;
+            }
+
+            // Si se borra todo el texto, mostrar el placeholder sin puntitos
+            if (txtPass.Text.Length == 0)
+            {
+                txtPass.Text = "CONTRASEÑA";
+                txtPass.UseSystemPasswordChar = false;
+                txtPass.ForeColor = Color.WhiteSmoke;
+
+                // Mover el cursor al inicio
+                txtPass.Select(0, 0);
+            }
+        }
+
+        private void txtPass_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPass.Text))
+            {
+                txtPass.Text = "CONTRASEÑA";
+                txtPass.ForeColor = Color.WhiteSmoke;
+                txtPass.UseSystemPasswordChar = false; 
+
+            }
+            else
+            {
+                // Mantener los puntitos si hay texto real
+                txtPass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimzar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void txtPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtPass.UseSystemPasswordChar == false && txtPass.Text == "CONTRASEÑA")
+            {
+                txtPass.Text = "";
+                txtPass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPass_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (txtPass.Text == "CONTRASEÑA" || string.IsNullOrEmpty(txtPass.Text))
+            {
+                txtPass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPass.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
