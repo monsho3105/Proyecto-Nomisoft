83024ded7302c3f7208d12860b76b8994ce022c2
DanielGonzalez314 <dg2621151812@gmail.com>
2025-11-18 21:45:02 -0500

    agregar emple terminado

--- Proyecto Nomisoft/Agregar_Emple.Designer.cs
+++ Proyecto Nomisoft/Agregar_Emple.Designer.cs
@@ -56,44 +56,54 @@
             // txt_Nombre1
             // 
             this.txt_Nombre1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Nombre1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_Nombre1.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Nombre1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.txt_Nombre1.Location = new System.Drawing.Point(286, 89);
+            this.txt_Nombre1.Location = new System.Drawing.Point(286, 102);
             this.txt_Nombre1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Nombre1.Name = "txt_Nombre1";
-            this.txt_Nombre1.Size = new System.Drawing.Size(180, 47);
+            this.txt_Nombre1.Size = new System.Drawing.Size(194, 40);
             this.txt_Nombre1.TabIndex = 1;
             // 
             // txt_Nombre2
             // 
             this.txt_Nombre2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Nombre2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_Nombre2.Font = new System.Drawing.Font("Sans Serif Collection", 7.799999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Nombre2.Location = new System.Drawing.Point(569, 93);
+            this.txt_Nombre2.Location = new System.Drawing.Point(568, 106);
             this.txt_Nombre2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Nombre2.Name = "txt_Nombre2";
-            this.txt_Nombre2.Size = new System.Drawing.Size(177, 42);
+            this.txt_Nombre2.Size = new System.Drawing.Size(177, 35);
             this.txt_Nombre2.TabIndex = 3;
             this.txt_Nombre2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txt_Apellido2
             // 
-            this.txt_Apellido2.Location = new System.Drawing.Point(1046, 101);
+            this.txt_Apellido2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Apellido2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Apellido2.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Apellido2.Location = new System.Drawing.Point(1137, 101);
             this.txt_Apellido2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Apellido2.Name = "txt_Apellido2";
-            this.txt_Apellido2.Size = new System.Drawing.Size(149, 22);
+            this.txt_Apellido2.Size = new System.Drawing.Size(149, 40);
             this.txt_Apellido2.TabIndex = 5;
             // 
             // txt_Apellido1
             // 
-            this.txt_Apellido1.Location = new System.Drawing.Point(823, 101);
+            this.txt_Apellido1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Apellido1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Apellido1.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Apellido1.Location = new System.Drawing.Point(859, 102);
             this.txt_Apellido1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Apellido1.Name = "txt_Apellido1";
-            this.txt_Apellido1.Size = new System.Drawing.Size(149, 22);
+            this.txt_Apellido1.Size = new System.Drawing.Size(149, 40);
             this.txt_Apellido1.TabIndex = 7;
             // 
             // Com_Box_Tipo_Doc
             // 
-            this.Com_Box_Tipo_Doc.Font = new System.Drawing.Font("Sans Serif Collection", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Com_Box_Tipo_Doc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.Com_Box_Tipo_Doc.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Com_Box_Tipo_Doc.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Com_Box_Tipo_Doc.FormattingEnabled = true;
             this.Com_Box_Tipo_Doc.Items.AddRange(new object[] {
             "Cedula de Ciudadania",
@@ -103,16 +113,17 @@
             "Visa ",
             "PPT",
             "PEP"});
-            this.Com_Box_Tipo_Doc.Location = new System.Drawing.Point(281, 162);
+            this.Com_Box_Tipo_Doc.Location = new System.Drawing.Point(286, 177);
             this.Com_Box_Tipo_Doc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Com_Box_Tipo_Doc.Name = "Com_Box_Tipo_Doc";
-            this.Com_Box_Tipo_Doc.Size = new System.Drawing.Size(228, 66);
+            this.Com_Box_Tipo_Doc.Size = new System.Drawing.Size(554, 59);
             this.Com_Box_Tipo_Doc.TabIndex = 11;
             this.Com_Box_Tipo_Doc.SelectedIndexChanged += new System.EventHandler(this.Com_Box_Tipo_Doc_SelectedIndexChanged);
             // 
             // txt_Numero_Doc
             // 
-            this.txt_Numero_Doc.Location = new System.Drawing.Point(791, 177);
+            this.txt_Numero_Doc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Numero_Doc.Location = new System.Drawing.Point(859, 189);
             this.txt_Numero_Doc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Numero_Doc.Name = "txt_Numero_Doc";
             this.txt_Numero_Doc.Size = new System.Drawing.Size(181, 22);
@@ -120,16 +131,18 @@
             // 
             // txt_Fecha_Nacimiento
             // 
-            this.txt_Fecha_Nacimiento.Location = new System.Drawing.Point(281, 255);
+            this.txt_Fecha_Nacimiento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Fecha_Nacimiento.Location = new System.Drawing.Point(286, 267);
             this.txt_Fecha_Nacimiento.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Fecha_Nacimiento.Name = "txt_Fecha_Nacimiento";
-            this.txt_Fecha_Nacimiento.Size = new System.Drawing.Size(181, 22);
+            this.txt_Fecha_Nacimiento.Size = new System.Drawing.Size(554, 22);
             this.txt_Fecha_Nacimiento.TabIndex = 15;
             this.txt_Fecha_Nacimiento.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // txt_Telefono
             // 
-            this.txt_Telefono.Location = new System.Drawing.Point(846, 267);
+            this.txt_Telefono.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Telefono.Location = new System.Drawing.Point(859, 267);
             this.txt_Telefono.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Telefono.Name = "txt_Telefono";
             this.txt_Telefono.Size = new System.Drawing.Size(181, 22);
@@ -138,15 +151,17 @@
             // 
             // txt_Correo
             // 
-            this.txt_Correo.Location = new System.Drawing.Point(273, 360);
+            this.txt_Correo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Correo.Location = new System.Drawing.Point(286, 356);
             this.txt_Correo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Correo.Name = "txt_Correo";
-            this.txt_Correo.Size = new System.Drawing.Size(181, 22);
+            this.txt_Correo.Size = new System.Drawing.Size(554, 22);
             this.txt_Correo.TabIndex = 19;
             // 
             // txt_Direccion
             // 
-            this.txt_Direccion.Location = new System.Drawing.Point(1004, 347);
+            this.txt_Direccion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Direccion.Location = new System.Drawing.Point(859, 356);
             this.txt_Direccion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Direccion.Name = "txt_Direccion";
             this.txt_Direccion.Size = new System.Drawing.Size(470, 22);
@@ -154,33 +169,37 @@
             // 
             // txt_Estado_Civil
             // 
-            this.txt_Estado_Civil.Location = new System.Drawing.Point(286, 449);
+            this.txt_Estado_Civil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Estado_Civil.Location = new System.Drawing.Point(286, 437);
             this.txt_Estado_Civil.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Estado_Civil.Name = "txt_Estado_Civil";
-            this.txt_Estado_Civil.Size = new System.Drawing.Size(181, 22);
+            this.txt_Estado_Civil.Size = new System.Drawing.Size(554, 22);
             this.txt_Estado_Civil.TabIndex = 23;
             // 
             // txt_Hijos
             // 
-            this.txt_Hijos.Location = new System.Drawing.Point(991, 437);
+            this.txt_Hijos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Hijos.Location = new System.Drawing.Point(859, 437);
             this.txt_Hijos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Hijos.Name = "txt_Hijos";
-            this.txt_Hijos.Size = new System.Drawing.Size(36, 22);
+            this.txt_Hijos.Size = new System.Drawing.Size(81, 22);
             this.txt_Hijos.TabIndex = 25;
             this.txt_Hijos.TextChanged += new System.EventHandler(this.textBox1_TextChanged_3);
             // 
             // txt_Cargo
             // 
-            this.txt_Cargo.Location = new System.Drawing.Point(283, 567);
+            this.txt_Cargo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Cargo.Location = new System.Drawing.Point(286, 565);
             this.txt_Cargo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Cargo.Name = "txt_Cargo";
-            this.txt_Cargo.Size = new System.Drawing.Size(181, 22);
+            this.txt_Cargo.Size = new System.Drawing.Size(554, 22);
             this.txt_Cargo.TabIndex = 27;
             this.txt_Cargo.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
             // 
             // txt_Departamento
             // 
-            this.txt_Departamento.Location = new System.Drawing.Point(791, 522);
+            this.txt_Departamento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Departamento.Location = new System.Drawing.Point(859, 565);
             this.txt_Departamento.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Departamento.Name = "txt_Departamento";
             this.txt_Departamento.Size = new System.Drawing.Size(181, 22);
@@ -188,15 +207,17 @@
             // 
             // txt_Fecha_Ingreso
             // 
-            this.txt_Fecha_Ingreso.Location = new System.Drawing.Point(286, 634);
+            this.txt_Fecha_Ingreso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Fecha_Ingreso.Location = new System.Drawing.Point(286, 643);
             this.txt_Fecha_Ingreso.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Fecha_Ingreso.Name = "txt_Fecha_Ingreso";
-            this.txt_Fecha_Ingreso.Size = new System.Drawing.Size(181, 22);
+            this.txt_Fecha_Ingreso.Size = new System.Drawing.Size(554, 22);
             this.txt_Fecha_Ingreso.TabIndex = 31;
             // 
             // text_Tipo_Contrato
             // 
-            this.text_Tipo_Contrato.Location = new System.Drawing.Point(804, 592);
+            this.text_Tipo_Contrato.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.text_Tipo_Contrato.Location = new System.Drawing.Point(859, 643);
             this.text_Tipo_Contrato.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.text_Tipo_Contrato.Name = "text_Tipo_Contrato";
             this.text_Tipo_Contrato.Size = new System.Drawing.Size(181, 22);
@@ -204,7 +225,8 @@
             // 
             // txt_Estado
             // 
-            this.txt_Estado.Location = new System.Drawing.Point(846, 669);
+            this.txt_Estado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Estado.Location = new System.Drawing.Point(859, 720);
             this.txt_Estado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Estado.Name = "txt_Estado";
             this.txt_Estado.Size = new System.Drawing.Size(181, 22);
@@ -212,15 +234,17 @@
             // 
             // txt_Salario
             // 
-            this.txt_Salario.Location = new System.Drawing.Point(299, 706);
+            this.txt_Salario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.txt_Salario.Location = new System.Drawing.Point(286, 720);
             this.txt_Salario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Salario.Multiline = true;
             this.txt_Salario.Name = "txt_Salario";
-            this.txt_Salario.Size = new System.Drawing.Size(181, 22);
+            this.txt_Salario.Size = new System.Drawing.Size(554, 22);
             this.txt_Salario.TabIndex = 37;
             // 
             // button_Agregar
             // 
-            this.button_Agregar.Location = new System.Drawing.Point(946, 778);
+            this.button_Agregar.Location = new System.Drawing.Point(859, 945);
             this.button_Agregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_Agregar.Name = "button_Agregar";
             this.button_Agregar.Size = new System.Drawing.Size(292, 77);
@@ -249,7 +273,7 @@
             "Manexka EPSI",
             "AIC EPSI",
             "Medimás EPS"});
-            this.comboBox_Eps.Location = new System.Drawing.Point(253, 803);
+            this.comboBox_Eps.Location = new System.Drawing.Point(286, 865);
             this.comboBox_Eps.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox_Eps.Name = "comboBox_Eps";
             this.comboBox_Eps.Size = new System.Drawing.Size(211, 24);
@@ -265,7 +289,7 @@
             "Protección",
             "Colfondos",
             "Skandia"});
-            this.comboBox_Pension.Location = new System.Drawing.Point(748, 803);
+            this.comboBox_Pension.Location = new System.Drawing.Point(869, 865);
             this.comboBox_Pension.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox_Pension.Name = "comboBox_Pension";
             this.comboBox_Pension.Size = new System.Drawing.Size(211, 24);
@@ -280,7 +304,7 @@
             "Colfondos",
             "Skandia",
             "Fondo Nacional del Ahorro"});
-            this.comboBox_Cesantias.Location = new System.Drawing.Point(253, 864);
+            this.comboBox_Cesantias.Location = new System.Drawing.Point(286, 945);
             this.comboBox_Cesantias.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox_Cesantias.Name = "comboBox_Cesantias";
             this.comboBox_Cesantias.Size = new System.Drawing.Size(211, 24);
@@ -291,7 +315,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1387, 875);
+            this.ClientSize = new System.Drawing.Size(1464, 1055);
             this.Controls.Add(this.comboBox_Cesantias);
             this.Controls.Add(this.comboBox_Pension);
             this.Controls.Add(this.comboBox_Eps);
